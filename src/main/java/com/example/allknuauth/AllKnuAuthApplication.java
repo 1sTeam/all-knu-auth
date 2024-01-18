13bf3f4035dc68f7a53b86b66bef9827f98f7318
kymokim <kymokim@gmail.com>
2024-01-18 22:33:36 +0900

    first commit

---
+++ src/main/java/com/example/allknuauth/AllKnuAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.allknuauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllKnuAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllKnuAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/allknuauth/AllKnuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allknuauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllKnuAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
